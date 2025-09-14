fedabe1554a663ae2ec181fe58ecd2d384e4af2c
MrJefferson29 <thejefferson29@gmail.com>
2025-09-14 22:15:07 +0100

    done

--- frontend/src/pages/ProductListing.jsx
+++ frontend/src/pages/ProductListing.jsx
@@ -163,6 +163,7 @@ const ProductListing = () => {
             return null;
           })()}
 
+
           {productsList.length > 0 ? (
             <main className="relative grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 pb-12">
               {productsList.map((glass) => (
